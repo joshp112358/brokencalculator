0b5e6076c2c659878914ef80cdccabc4cb3e0b2d
Joshua Paik <joshuapaik@Joshuas-MBP.home>
2018-07-24 16:39:12 -0700

    heavily edited

--- sketch.js
+++ sketch.js
@@ -2,17 +2,18 @@ var calcLevelSketch = function(p) {
 
   p.calcButtons = [];
   p.calcInput;
-  var calcButton
-  var calcDeltaX = 40;
-  var calcDeltaY = 50;
-  var calcShow = true;
-  // how can I make brokenKeys and target a input of calcLevel
-  p.brokenKeys;
-	p.target;
-	var score=0;
-	var scoreElem;
+  p.brokenKeys = ['7', '8']
+  p.target = 0;
+  var targElem;
+  p.score = 0;
+  var scorElem;
+  var backButton;
+  var timerElem;
+  var time = 60;
+
 
   p.makeCalcButtons = function() {
+    p.calcButtons = [];
     p.calcButtons.push(p.calcInput = p.createInput('').position(100, 140));
     p.calcButtons.push(p.createButton('7').position(100, 200));
     p.calcButtons.push(p.createButton('8').position(160, 200));
@@ -26,144 +27,152 @@ var calcLevelSketch = function(p) {
     p.calcButtons.push(p.createButton('2').position(160, 320));
     p.calcButtons.push(p.createButton('3').position(220, 320));
     p.calcButtons.push(p.createButton('-').position(280, 320));
-    p.calcButtons.push(p.createButton('CE').position(100, 380).mousePressed(clear));
+    p.calcButtons.push(p.createButton('CE').position(100, 380));
     p.calcButtons.push(p.createButton('0').position(160, 380));
-    p.calcButtons.push(p.createButton('=').position(220, 380).mousePressed(equals));
+    p.calcButtons.push(p.createButton('=').position(220, 380));
     p.calcButtons.push(p.createButton('+').position(280, 380));
 
-		p.calcInput.style('width', '225px')
-							.style('height', '35px')
-							.style('background-color', "rgb(60,900,60)")
-							.style("font-size","2em")
-							.style("align-content","right");
+    p.calcInput.style('width', '225px')
+      .style('height', '35px')
+      .style('background-color', "rgb(60,900,60)")
+      .style("font-size", "2em")
+      .style("align-content", "right");
 
-		for (i = 0; i < p.calcButtons.length; i++) {
-      if (p.calcButtons[i].html() === '=' || p.calcButtons[i].html() === 'CE'){}
-      else if (p.brokenKeys.indexOf(p.calcButtons[i].html()) !== -1) {} else {
-        p.calcButtons[i].mousePressed(add)
-      }
+
+    for (let i = 1; i < p.calcButtons.length; i++) {
+      p.calcButtons[i].style('width', '50px')
+        .style('height', '50px')
+        .style('background-color', "rgb(60,900, 60)")
+        .mousePressed(add);
     }
-
-  }
-	function drawXs() {
-		if (p.brokenKeys.indexOf('7') !== -1){
-			X(100, 200)
-		}if (p.brokenKeys.indexOf('8') !== -1){
-			X(160, 200)
-		}if (p.brokenKeys.indexOf('9') !== -1){
-			X(220, 200)
-		}if (p.brokenKeys.indexOf('/') !== -1){
-			X(280, 200)
-		}if (p.brokenKeys.indexOf('4') !== -1){
-			X(100, 260)
-		}if (p.brokenKeys.indexOf('5') !== -1){
-			X(160, 260)
-		}if (p.brokenKeys.indexOf('6') !== -1){
-			X(220, 260)
-		}if (p.brokenKeys.indexOf('*') !== -1){
-			X(280, 260)
-		}if (p.brokenKeys.indexOf('1') !== -1){
-			X(100, 320)
-		}if (p.brokenKeys.indexOf('2') !== -1){
-			X(160, 320)
-		}if (p.brokenKeys.indexOf('3') !== -1){
-			X(220, 320)
-		}if (p.brokenKeys.indexOf('-') !== -1){
-			X(280, 320)
-		}if (p.brokenKeys.indexOf('7') !== -1){
-			X(100, 380)
-		}if (p.brokenKeys.indexOf('8') !== -1){
-			X(160, 380)
-		}if (p.brokenKeys.indexOf('9') !== -1){
-			X(220, 380)
-		}if (p.brokenKeys.indexOf('/') !== -1){
-			X(280, 380)
-		}
-	}
-
-  function X(i, j) {
-    p.stroke('red');
-    p.strokeWeight(4);
-    p.line(i, j, i + 50, j + 50);
-    p.line(i + 50, j, i, j + 50);
+    p.calcButtons[13].mousePressed(zeroed)
+    p.calcButtons[15].mousePressed(equals)
   }
 
-  var targetElem;
-  function drawTimer() {}
-  var backButton;
 
-  p.setup = function() {
-    p.createCanvas(500, 500).parent("calculatorScreen");
-    p.makeCalcButtons();
-		scoreElem = p.createDiv().position(100,50)
-								.style("color", "white")
-								.style("font-size","1.5em");
-		targetElem = p.createDiv().position(100, 80)
-																		.style("color", "white")
-																		.style("font-size","1.5em");
 
-    p.background('orange');
-    backButton = p.createButton('Back').mousePressed(p.switchScreen)
-                                      .position(400,100)
-
-  }
-
-  p.switchScreen = function(){
-    document.getElementById("levelingScreen").style.display = "block";
-    document.getElementById("calculatorScreen").style.display = "none";
-  }
-
-  p.draw = function() {
-		scoreElem.html('Score: '+ score)
-    for (i = 1; i < p.calcButtons.length; i++) {
-			if (p.brokenKeys.indexOf(p.calcButtons[i].html()) === -1){
-				p.calcButtons[i].style('width', '50px')
-											.style('height', '50px')
-											.style('background-color', "rgb(60,900, 60)")
-											.style("font-size","2em");
-			} else if (p.calcButtons[i].html()==='='){
-				p.calcButtons[i].style('width', '50px')
-											.style('height', '50px')
-											.style('background-color', "rgb(60,900, 60)")
-											.style("font-size","2em");
-			} else {
-				p.calcButtons[i].style('width', '50px')
-											.style('height', '50px')
-											.style('background-color', "rgb(256,0,0)")
-											.style("font-size","2em");
-			}
-		}
-
-    targetElem.html("Target: "+p.target)
-  }
-
-  function add() {
-    p.calcInput.value(p.calcInput.value() + this.html());
+  p.makeBrokenKeys = function(){
+    if (p.brokenKeys.indexOf('7')!==-1){
+      p.calcButtons[1].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('8')!==-1){
+      p.calcButtons[2].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('9')!==-1){
+      p.calcButtons[3].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('/')!==-1){
+      p.calcButtons[4].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('4')!==-1){
+      p.calcButtons[5].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('5')!==-1){
+      p.calcButtons[6].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('6')!==-1){
+      p.calcButtons[7].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('*')!==-1){
+      p.calcButtons[8].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('1')!==-1){
+      p.calcButtons[9].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('2')!==-1){
+      p.calcButtons[10].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('3')!==-1){
+      p.calcButtons[11].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('-')!==-1){
+      p.calcButtons[12].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('0')!==-1){
+      p.calcButtons[14].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
+    if (p.brokenKeys.indexOf('+')!==-1){
+      p.calcButtons[16].style('background-color', "rgb(256,0,0)")
+      .mousePressed(donothing);
+    }
   }
 
   function equals() {
-			// for every successful entry, add a point.
     p.calcInput.value(eval(p.calcInput.value()));
     if (p.calcInput.value() == p.target){
-      clear();
-      score += 1;
+      zeroed();
+      p.score += 1;
     }
     else{
-      clear();
-      p.stroke('white');
-
+      zeroed();
     }
-	}
-  function clear() {
+  }
+
+  function add(){
+    p.calcInput.value(p.calcInput.value() + this.html());
+  }
+
+  function zeroed() {
     p.calcInput.value("");
+  }
+
+  function donothing() {}
+
+  p.switchScreen = function(){
+  document.getElementById("levelingScreen").style.display = "block";
+  document.getElementById("calculatorScreen").style.display = "none";
+}
+
+  var counter = 0;
+  var interval;
+
+  p.startTimer = function(){
+    interval = setInterval(timeIt, 1000);
+  }
+  function timeIt(){
+    counter++;
+    timerElem.html("Time "+eval(time-counter));
+    if (counter == 10){
+      clearInterval(interval);
+      counter = 0;
+    }
+  }
+  p.setup = function() {
+    p.createCanvas(500, 500);
+    p.background('orange');
+    p.makeCalcButtons();
+    scoreElem = p.createDiv().position(100,50);
+    targElem = p.createDiv().position(100,80);
+    backButton = p.createButton('Back').mousePressed(p.switchScreen).position(400,100);
+    timerElem = p.createDiv("Time "+time).position(250,80);
+  }
+
+  p.draw = function() {
+    scoreElem.html('Score: '+ p.score)
+    targElem.html('Target:' + p.target)
   }
 }
 
+
+
+
 var levelSelectionSketch = function(s){
-  var l1;
   s.setup = function(){
     s.createCanvas(500,500);
-    s.background('black');
+    s.background('orange');
     s.createButton("level1").position(100,100).mousePressed(s.switchScreen1);
     s.createButton("level2").position(180,100).mousePressed(s.switchScreen2);
 
