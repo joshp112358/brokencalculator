055d22f7b68eb8136776fee9e849713463a254f5
Joshua Paik <joshuapaik@Joshuas-MacBook-Pro.local>
2018-08-29 16:29:29 -0700

    fixed max score

--- initialScreen.js
+++ initialScreen.js
@@ -1,7 +1,7 @@
 var initScreenSketch = function(p){
   p.setup = function(){
     p.createCanvas(500,500);
-    p.background('#43f2c3');
+    p.background('pink');
     p.textSize(25);
     p.text("Broken Calculators", 150, 220);
     p.createButton("Start Game").position(220,250).mousePressed(showHide);
